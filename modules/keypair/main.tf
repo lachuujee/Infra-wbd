f6b6691e1ac7c05c471b6cf16ef49921c42942b9
lachuujee <lakshmi.wl240@gmail.com>
2025-09-26 10:05:22 +0530

    Update main.tf

--- modules/keypair/main.tf
+++ modules/keypair/main.tf
@@ -1,13 +1,15 @@
 # 1) Generate private/public key (no .pem written to disk)
 resource "tls_private_key" "this" {
+  count     = var.enabled ? 1 : 0
   algorithm = var.algorithm
   rsa_bits  = var.algorithm == "RSA" ? var.rsa_bits : null
 }
 
 # 2) Register public key in EC2
 resource "aws_key_pair" "this" {
+  count      = var.enabled ? 1 : 0
   key_name   = local.key_name
-  public_key = tls_private_key.this.public_key_openssh
+  public_key = tls_private_key.this[0].public_key_openssh
   tags       = local.common_tags
 
   lifecycle {
@@ -15,17 +17,17 @@ resource "aws_key_pair" "this" {
   }
 }
 
-# 3) Secrets Manager secret (uses AWS-managed KMS by default)
+# 3) Secrets Manager secret (name EXACTLY equals the keypair name)
 resource "aws_secretsmanager_secret" "pk" {
-  name                    = "${local.key_name}-private-key"
+  count                   = var.enabled ? 1 : 0
+  name                    = local.key_name              # e.g., SBX_INTAKE_ID_001-keypair
   recovery_window_in_days = 30
   tags                    = local.common_tags
 }
 
 # 4) Store PEM string in the secret
 resource "aws_secretsmanager_secret_version" "pkv" {
-  secret_id     = aws_secretsmanager_secret.pk.id
-  secret_string = tls_private_key.this.private_key_pem
-  depends_on    = [aws_secretsmanager_secret.pk]
+  count         = var.enabled ? 1 : 0
+  secret_id     = aws_secretsmanager_secret.pk[0].id
+  secret_string = tls_private_key.this[0].private_key_pem
 }
-
