b71fe7af17c5a5ce4886e8fa49a2a249ece9df00
lachuujee <lakshmi.wl240@gmail.com>
2025-09-26 09:45:32 +0530

    Update main.tf

--- modules/keypair/main.tf
+++ modules/keypair/main.tf
@@ -1,1 +1,31 @@
+# 1) Generate private/public key (no .pem written to disk)
+resource "tls_private_key" "this" {
+  algorithm = var.algorithm
+  rsa_bits  = var.algorithm == "RSA" ? var.rsa_bits : null
+}
+
+# 2) Register public key in EC2
+resource "aws_key_pair" "this" {
+  key_name   = local.key_name
+  public_key = tls_private_key.this.public_key_openssh
+  tags       = local.common_tags
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# 3) Secrets Manager secret (uses AWS-managed KMS by default)
+resource "aws_secretsmanager_secret" "pk" {
+  name                    = "${local.key_name}-private-key"
+  recovery_window_in_days = 30
+  tags                    = local.common_tags
+}
+
+# 4) Store PEM string in the secret
+resource "aws_secretsmanager_secret_version" "pkv" {
+  secret_id     = aws_secretsmanager_secret.pk.id
+  secret_string = tls_private_key.this.private_key_pem
+  depends_on    = [aws_secretsmanager_secret.pk]
+}
 
