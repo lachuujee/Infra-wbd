0cc8321ca8689c507d1414b19c2c949c4bd6eeeb
lachuujee <lakshmi.wl240@gmail.com>
2025-09-26 08:54:03 +0530

    Create providers.tf

---
+++ live/sandbox/users/intake_001/keypair/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.60"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "wbd-tf-state-sandbox"
+    key     = "wbd/sandbox/keypair/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
