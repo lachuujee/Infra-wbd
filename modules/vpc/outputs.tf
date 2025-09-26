a676ecf8abae47a8c8f717cc5b678b706344086b
lachuujee <lakshmi.wl240@gmail.com>
2025-09-26 15:35:12 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,27 +1,24 @@
 output "vpc_id" {
-  value       = var.enabled ? aws_vpc.this[0].id : null
   description = "VPC ID"
+  value       = try(aws_vpc.this[0].id, null)
 }
 
 output "vpc_cidr_block" {
-  value       = var.enabled ? aws_vpc.this[0].cidr_block : null
   description = "VPC CIDR"
+  value       = try(aws_vpc.this[0].cidr_block, null)
 }
 
 output "public_subnet_ids" {
-  value       = var.enabled ? [for k, s in aws_subnet.public : s.id] : []
   description = "Public subnet IDs"
+  value       = [for k, s in aws_subnet.public : s.id]
 }
 
 output "private_subnet_ids" {
+  description = "Private subnet IDs (deterministic order by key)"
   value       = [for k, s in aws_subnet.private : s.id]
-  description = "Private subnet IDs"
 }
 
 output "private_subnet_ids_by_role" {
-  value = {
-    for k, s in aws_subnet.private :
-    k => s.id
-  }
   description = "Map of private subnets by role key (e.g., app-a, api-b, db-a)"
+  value       = { for k, s in aws_subnet.private : k => s.id }
 }
