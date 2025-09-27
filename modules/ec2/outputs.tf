ae6ba28012c6477a44381eeee799ab8a72dca05c
lachuujee <lakshmi.wl240@gmail.com>
2025-09-28 01:43:23 +0530

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,14 +1,29 @@
-output "instance_ids" {
-  description = "EC2 instance IDs"
-  value       = [for i in aws_instance.this : i.id]
-}
-
-output "instance_private_ips" {
-  description = "Private IPs of instances"
-  value       = [for i in aws_instance.this : i.private_ip]
-}
-
 output "security_group_id" {
-  description = "Security group protecting these instances"
-  value       = var.enabled ? aws_security_group.app[0].id : null
+  description = "EC2 SG ID"
+  value       = length(aws_security_group.app_sg) > 0 ? aws_security_group.app_sg[0].id : null
+}
+
+output "instance_ids" {
+  description = "Launched instance IDs"
+  value       = [for i in aws_instance.app : i.id]
+}
+
+output "private_ips" {
+  description = "Launched instance private IPs"
+  value       = [for i in aws_instance.app : i.private_ip]
+}
+
+output "effective_ami" {
+  description = "AMI actually used for launch"
+  value       = local.effective_ami
+}
+
+output "key_name_used" {
+  description = "KeyPair name used (if any)"
+  value       = local.key_name
+}
+
+output "iam_profile_used" {
+  description = "IAM instance profile used (if any)"
+  value       = local.iam_instance_profile
 }
