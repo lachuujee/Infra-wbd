dc681fd3e892357a02e5a41116bad00f9c65b2c7
lachuujee <lakshmi.wl240@gmail.com>
2025-09-27 23:56:09 +0530

    Update main.tf

--- modules/keypair/main.tf
+++ modules/keypair/main.tf
@@ -1,11 +1,15 @@
-# 1) Generate private/public key (no .pem written to disk)
+############################################
+# 1) Generate private/public key (in memory)
+############################################
 resource "tls_private_key" "this" {
   count     = var.enabled ? 1 : 0
   algorithm = var.algorithm
   rsa_bits  = var.algorithm == "RSA" ? var.rsa_bits : null
 }
 
-# 2) Register public key in EC2
+############################################
+# 2) Register EC2 Key Pair
+############################################
 resource "aws_key_pair" "this" {
   count      = var.enabled ? 1 : 0
   key_name   = local.key_name
@@ -17,15 +21,19 @@ resource "aws_key_pair" "this" {
   }
 }
 
-# 3) Secrets Manager secret (name EXACTLY equals the keypair name)
+############################################
+# 3) Secrets Manager secret == key name
+############################################
 resource "aws_secretsmanager_secret" "pk" {
   count                   = var.enabled ? 1 : 0
-  name                    = local.key_name              # e.g., SBX_INTAKE_ID_001-keypair
+  name                    = local.key_name
   recovery_window_in_days = 30
   tags                    = local.common_tags
 }
 
-# 4) Store PEM string in the secret
+############################################
+# 4) Store PEM in the secret
+############################################
 resource "aws_secretsmanager_secret_version" "pkv" {
   count         = var.enabled ? 1 : 0
   secret_id     = aws_secretsmanager_secret.pk[0].id
